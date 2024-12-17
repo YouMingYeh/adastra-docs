2e4c88d592f318031cb16fc83cc279a38a91d0f6
YouMingYeh <b10705052@ntu.edu.tw>
2024-12-17 11:18:07 +0800

    Add favicon link and update metadata with logo image in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./global.css";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Providers } from "./providers";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -11,11 +12,15 @@ export const metadata = {
   title: "Adastra 使用手冊",
   description:
     "這是 Adastra 的使用手冊，你可以在這裡找到所有如何使用 Adastra 的資訊。",
-};
+  image: "/logo.png",
+} as Metadata;
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className="flex min-h-screen flex-col">
         <Providers>{children}</Providers>
       </body>
